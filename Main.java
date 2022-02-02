780b1da1e49336945cc5a9ee7368853b9ed0f144
anmol008 <anmolbhargava97@gmail.com>
2022-02-02 22:57:05 +0530

    abc

---
+++ Main.java
@@ -0,0 +1,26 @@
+package oopsFms;
+public class Main {
+
+    public static void main(String[] args) {
+        //Main.java have to types to run, Ticket regular and Touristticket
+
+        Ticket touristTicket = new TouristTicket("920092", "India", "England",
+                "DepartureInfo", "ArrivalInfo",
+                "32", 7600, false, null, null,
+                "Lords Hotel", new String[]{""});
+
+        Ticket regularTicket = new RegularTicket("989372", "Mumbai",
+                "Bhopal", "DepartureInfo", "ArrivalInfo",
+                "23", 2450, false, null, null, "lunch");
+
+
+        printTicketDetails(touristTicket);
+
+        printTicketDetails(regularTicket);
+    }
+
+    public static void printTicketDetails(Ticket ticket) {
+        System.out.println(ticket.getPnr());
+    }
+
+}
